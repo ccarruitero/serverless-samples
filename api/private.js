f0bcb44e57cdb9454a6d2609f75d46af06dc31cb
César Carruitero <ccarruitero@protonmail.com>
2019-09-27 13:47:00 -0500

    update hapi

--- api/private.js
+++ api/private.js
@@ -1,52 +1,52 @@
-const Hapi = require('hapi');
-const Joi = require('joi');
+const Hapi = require('@hapi/hapi');
+const Joi = require('@hapi/joi');
 const jwt = require('hapi-auth-jwt2');
 
-const server = new Hapi.Server();
-
-server.connection({
-  port: process.env.PORT || 3010,
-  routes: {
-    cors: {
-      // change this for production
-      origin: ['*']
-    }
-  }
-});
-
-const registerRoutes = () => {
-  server.route({
-    method: 'GET',
-    path: '/api/private',
-    config: {
-      auth: 'jwt',
-      handler: (req, res) => {
-        res({
-          message: 'Hello from a private endpoint!'
-        });
+const init = async () => {
+  const server = new Hapi.Server({
+    port: process.env.PORT || 3010,
+    routes: {
+      cors: {
+        // change this for production
+        origin: ['*']
       }
     }
   });
-};
 
-const validateUser = (decoded, request, callback) => {
-  // This is a simple check that the `sub` exists
-  // Modify it to suit the needs of your application
-  if (decoded && decoded.sub) {
-    if (decoded.scope)
-      return callback(null, true, {
-        scope: decoded.scope.split(' ')
-      });
+  const registerRoutes = () => {
+    server.route({
+      method: 'GET',
+      path: '/api/private',
+      config: {
+        auth: 'jwt',
+        handler: (req, res) => {
+          res({
+            message: 'Hello from a private endpoint!'
+          });
+        }
+      }
+    });
+  };
 
-    return callback(null, true);
-  }
+  const validateUser = async (decoded, request) => {
+    // This is a simple check that the `sub` exists
+    // Modify it to suit the needs of your application
+    if (decoded && decoded.sub) {
+      if (decoded.scope) {
+        return {
+          isValid: true,
+          scope: decoded.scope.split(' ')
+        };
+      }
 
-  return callback(null, false);
-};
+      return { isValid: true };
+    }
 
-server.register(jwt, err => {
-  if (err) throw err;
-  server.auth.strategy('jwt', 'jwt', 'required', {
+    return { isValid: false };
+  };
+
+  await server.register(jwt);
+  server.auth.strategy('jwt', 'jwt', {
     // verify the access token against local secret key
     key: process.env.AUTH0_HS256_SECRET,
     verifyOptions: {
@@ -54,14 +54,18 @@ server.register(jwt, err => {
       issuer: `https://${process.env.AUTH0_DOMAIN}/`,
       algorithms: ['HS256']
     },
-    validateFunc: validateUser
+    validate: validateUser
   });
+  server.auth.default('jwt');
+
   registerRoutes();
-});
 
-server.start(err => {
-  if (err) throw err;
+  await server.start();
   console.info(`Server started at ${server.info.uri}`);
-});
 
-module.exports = server;
+  return server;
+};
+
+const app = init();
+
+module.exports = app;
