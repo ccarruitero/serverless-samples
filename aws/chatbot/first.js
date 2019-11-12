ca3beea66925cb1f61291bf92f1736f9f46c402d
César Carruitero <ccarruitero@protonmail.com>
2019-11-11 23:08:41 -0500

    WIP

--- aws/chatbot/first.js
+++ aws/chatbot/first.js
@@ -1,3 +1,25 @@
+import {
+   FacebookAdapter,
+   FacebookEventTypeMiddleware
+} from 'botbuilder-adapter-facebook';
+import { Botkit } from 'botkit';
+
+const adapter = new FacebookAdapter({
+   verify_token: process.env.FACEBOOK_VERIFY_TOKEN,
+   app_secret: process.env.FACEBOOK_APP_SECRET,
+   access_token: process.env.FACEBOOK_ACCESS_TOKEN
+});
+
+adapter.use(new FacebookEventTypeMiddleware());
+
+const controller = new Botkit({
+  adapter
+});
+
+controller.on('message', async (bot, message) => {
+  await bot.reply(message, 'Hallo, I heard a message!');
+});
+
 // eslint-disable-next-line import/prefer-default-export
 export const hello = (event, context, callback) => {
   const p = new Promise(resolve => {
